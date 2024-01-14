da94a5fb82b0147f6e855747ae975b1a9b21175f
PeterTKovacs <kptzeg@gmail.com>
2024-01-14 18:38:29 +0100

    fix data to device

--- char_experiment.py
+++ char_experiment.py
@@ -2,7 +2,10 @@ from ood.models.custom_models import train_step, valid_step
 from torch.utils.data import DataLoader
 import torch
 
-def train_and_measure_loop(model,optimizer,train_loader,ds_measure,measurement_interval=1,return_loss=False):
+def convert_batch(batch,device):
+    return (batch[0].to(device),batch[1].to(device))
+
+def train_and_measure_loop(model,optimizer,train_loader,ds_measure,device="cpu",measurement_interval=1,return_loss=False):
     """
     generic train loop
     we assume that model is already sent to device
@@ -12,20 +15,22 @@ def train_and_measure_loop(model,optimizer,train_loader,ds_measure,measurement_i
     measurements=[]
 
     m_dl=DataLoader(ds_measure,batch_size=len(ds_measure),shuffle=False)
-    full_meas_batch=next(iter(m_dl))
+    full_meas_batch=convert_batch(next(iter(m_dl)),device)
 
-    for i,batch in enumerate(iter(train_loader)):
+    for i,_batch in enumerate(iter(train_loader)):
+        batch=convert_batch(_batch,device)
         if return_loss:
             loss.append(train_step(model,batch,optimizer,return_loss))
         else:
             train_step(model,batch,optimizer,return_loss)
 
         if i%measurement_interval==0:
-            measurements.append((i,model.valid_loss_standalone(full_meas_batch)))
+            with torch.no_grad():
+                measurements.append((i,model.valid_loss_standalone(full_meas_batch).detach().cpu().numpy()))
 
     return measurements,loss
 
-def valid_loop(model,val_loader):
+def valid_loop(model,val_loader,device="cpu"):
 
     """
     generic valid loop
@@ -35,7 +40,8 @@ def valid_loop(model,val_loader):
     avg_loss=0
     n=0
     with torch.no_grad():
-        for batch in iter(val_loader):
+        for _batch in iter(val_loader):
+            batch=convert_batch(_batch,device)
             avg_loss+=valid_step(model,batch)*batch[0].shape[0]
             n+=batch[0].shape[0]
 
