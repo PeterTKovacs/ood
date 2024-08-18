e7f244446ce605f92e5ec79e73991eb5888fb07e
PeterTKovacs <kptzeg@gmail.com>
2024-08-18 14:15:02 +0200

    unfuck import

--- cifar_experiment_setup.py
+++ cifar_experiment_setup.py
@@ -4,8 +4,8 @@ import json
 from typing import List, Callable
 import os
 import shutil
-from . import IgniteModel
-from . import make_cnn
+from custom_models import IgniteModel
+from mcnn import make_cnn
 import torch
 
 from ignite.engine import create_supervised_trainer, create_supervised_evaluator, Events
